acc5c5eb36730af6e55cc0c7dee8932ef9493ddd
sourav-kr14 <souravkr93@gmail.com>
2026-05-05 23:05:14 +0530

    update: dependecy and import added for toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AdminLayout from "./components/admin/layout/AdminLayout";
 import AdminDashboard from "./components/admin/dashboard/AdminDashboard";
 import AdminUsers from "./components/admin/dashboard/AdminUsers";
 import AdminReports from "./pages/admin/AdminReports";
-
+import { Toaster } from "sonner";
 import { AdminRoute, UserRoute } from "./routes/ProtectedRoutes";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SettingsLayout from "./components/settings/SettingsLayout";
@@ -17,6 +17,7 @@ import ResetPassword from "./pages/Auth/ResetPassword";
 export default function App() {
   return (
     <BrowserRouter>
+      <Toaster theme="system" position="top-right" richColors />
       <Routes>
         {/* AUTH */}
         <Route path="/" element={<Navigate to="/signup" />} />
